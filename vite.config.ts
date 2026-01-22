e99d143c6652f852c9a6d40d5225a69686b497bf
Анна Матвеева <annamatveeva-adv@yandex.ru>
2026-01-22 22:55:08 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Пустая строка '' или './' заставляет Vite использовать относительные пути,
-  // что критически важно для работы на GitHub Pages в подпапках.
-  base: '',
+  // Относительный путь гарантирует, что ресурсы загрузятся правильно на любом URL
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
